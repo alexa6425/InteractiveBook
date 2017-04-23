4fbbe24c9ea68c703a58394b4f5ff1e553d8fca5
Alexander Pickering <alexandermpickering@gmail.com>
2017-04-22 21:35:13 -0400

    Added some stuff

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# InteractiveBook
+# Interactive Book
+
+A collection of short stories from a Digital Media class of 2017 at the University of Pittsburgh.
+
+These are the files needed outside of each student's individual fables.
--- bookify.js
+++ bookify.js
@@ -30,9 +30,47 @@ function makenav(){
 	var navdiv = document.getElementById("nav");
 	var whereami = window.location;
 	var fable_order = [
-		"/fable1.html",
-		"file:///B:/documents/university/spring17/DigitalMedia/InteractiveBook/test.html",
-		"/fable2.html",
+		"http://aesopht.ml/",
+		"http://www.pitt.edu/~nds35/forward.html",
+		"http://aesopht.ml/contents.html",
+		
+		"http://www.pitt.edu/~acs162/CDM/InteractiveBook/fox.html",
+		"http://www.pitt.edu/~acs162/CDM/InteractiveBook/donkey.html",
+		"http://www.pitt.edu/~ejc58/fable2.html",
+		"http://www.pitt.edu/~ejc58/index.html",
+		"http://www.pitt.edu/~brp76/fable1.html",
+		"http://www.pitt.edu/~brp76/fable2.html",
+		"http://www.pitt.edu/~aay11/queen.html",
+		"http://www.pitt.edu/~kjb103/shirts.html",
+		"http://www.pitt.edu/~kjb103/FreshmanDonkey.html",
+		"http://www.pitt.edu/~cmp114/ClassClown.html",
+		"http://www.pitt.edu/~cmp114/SpellingBee.html",
+		"http://www.pitt.edu/~aay11/peacock.html",
+		"http://www.pitt.edu/~jpr47/web%20fables/FINAL/pianist.html",
+		"http://www.pitt.edu/~jpr47/web%20fables/FINAL/crowgale.html",
+		"http://cogarr.net/files/alex/02/template.html",
+		"http://cogarr.net/files/alex/01/template.html",
+		"http://www.pitt.edu/~lmm192/rabbit&uniform.html",
+		"http://pitt.edu/~jsk60/Fable1.html",
+		"http://www.pitt.edu/~lmm192/duck&elephant.html",
+		"http://pitt.edu/~jsk60/Fable2.html",
+		"http://www.pitt.edu/~chs187/hippo.html",
+		"http://www.pitt.edu/~chs187/sheep2.html",
+		"http://www.pitt.edu/~acs162/CDM/InteractiveBook/mouse.html",
+		"http://www.pitt.edu/~erg52/fable2.html",
+		"http://www.pitt.edu/~acs162/CDM/InteractiveBook/Horse.html",
+		"http://www.pitt.edu/~nds35/pb.html",
+		"http://www.pitt.edu/~rml50/Crow%20Final.html",
+		"http://pitt.edu/~mrp82/athleteFable.html",
+		"http://www.pitt.edu/~rml50/elephant%20final.html",
+		"http://pitt.edu/~mrp82/cheetahFable.html",
+		"http://www.pitt.edu/~erg52/fable1.html",
+		"http://www.pitt.edu/~lcm38/catfox.html",
+		"http://www.pitt.edu/~lcm38/Mouse.html",
+		"http://www.pitt.edu/~mas559/CDM/MovingStory/Cat_And_Dogs.html",
+		"http://www.pitt.edu/~mas559/CDM/MovingStory/Possum_and_Piano.html",
+		
+		"http://www.pitt.edu/~nds35/Afterword.html",
 	];
 	var wheretonext,wherewasi;
 	for(var i = 0; i < fable_order.length; i++){
@@ -42,12 +80,20 @@ function makenav(){
 			break;
 		}
 	}
-	navdiv.innerHTML = `
-<ul class="nav" sytle="margins:auto;">
-<li id="backButton"><a href="` + wherewasi + `" target="_self">&lsaquo; BACK</a></li>
-<li id="nextButton"><a href="` + wheretonext + `" target="_self">NEXT &rsaquo;</a></li>
-<li id="tableOfContents"> <a href="fable2.html">TABLE OF CONTENTS</a></li>
-</ul>
-`;
+	var build = `<ul class="nav" sytle="margins:auto;">`;
+	if(wherewasi != null){
+		build += `<li id="backButton"><a href="` + wherewasi + `" target="_self">&lsaquo; BACK</a></li>`;
+	}else{
+		build += `<li id="backButton">&lsaquo; BACK</li>`;
+	}
+	if(wheretonext != null){
+		build += `<li id="nextButton"><a href="` + wheretonext + `" target="_self">NEXT &rsaquo;</a></li>`;
+	}else{
+		build += `<li id="nextButton">NEXT &rsaquo;</li>`;
+	}
+
+	build += `<li id="tableOfContents"> <a href="http://aesopht.ml/contents.
+html">TABLE OF CONTENTS</a></li></ul>`;
+	   navdiv.innerHTML = build;
 }
 window.onload = makenav;
